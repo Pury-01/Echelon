9c09f484031436661ce4e0cfc0ba6959847b0a15
Pury-01 <puryokumu@gmail.com>
2024-08-26 21:51:10 +0300

    fixed syntax, style and typo errors

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,10 @@
-import App from './App';                                        import './index.css';                                                                                                           ReactDOM.render(
-        <React.SttrictMode>
-        <App />
-        </React.StrictMode>,                                            document.getElementById('root')                         );                                                              ~                                                               ~              
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
