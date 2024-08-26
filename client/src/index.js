0e380271cc713692e3ad4abc5c08a748e1fb705b
Pury-01 <puryokumu@gmail.com>
2024-08-26 17:54:44 +0300

    directories and packages for client side

---
+++ client/src/index.js
@@ -0,0 +1,4 @@
+import App from './App';                                        import './index.css';                                                                                                           ReactDOM.render(
+        <React.SttrictMode>
+        <App />
+        </React.StrictMode>,                                            document.getElementById('root')                         );                                                              ~                                                               ~              
